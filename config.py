81907373fd14c07def7116682e76915e3a86dd12
Nicolas Anguiano <nico@alchemyforexperts.com>
2025-03-28 13:28:17 -0500

    detecting environment by FLASK_ENV

--- config.py
+++ config.py
@@ -1,9 +1,8 @@
 import os
-import socket
 
 def is_production():
-    """Check if we're running on the production server by hostname"""
-    return 'agenticstudio.co' in socket.gethostname()
+    """Check if we're running in production using FLASK_ENV environment variable"""
+    return os.environ.get('FLASK_ENV') == 'production'
 
 # Set base path depending on environment
 BASE_PATH = '/100DoC/day1-band' if is_production() else ''
--- gunicorn_config.py
+++ gunicorn_config.py
@@ -6,3 +6,6 @@ forwarded_allow_ips = '*'  # Trust X-Forwarded-* headers
 secure_scheme_headers = {'X-FORWARDED-PROTOCOL': 'ssl', 'X-FORWARDED-PROTO': 'https', 'X-FORWARDED-SSL': 'on'}
 accesslog = "access.log"  # Log file for access logs
 errorlog = "error.log"    # Log file for error logs
+
+# Set Flask environment
+raw_env = ["FLASK_ENV=production"]
